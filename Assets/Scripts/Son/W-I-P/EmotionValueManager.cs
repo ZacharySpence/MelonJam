ca980d7547a23d8d4d062d648158460bd4efd942
MardookStone <BackTrack1984@hotmail.com>
2024-12-28 08:16:02 +0000

    Added Emotion Values
    1.made the ReactionSO to hold the hot/cold/neutral messages
    
    2.Checks the happy/angry emotion then chooses one of 4 replies (need to put into OptionSO based off hot/cold

---
+++ Assets/Scripts/Son/W-I-P/EmotionValueManager.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EmotionValueManager : MonoBehaviour
+{
+    public static EmotionValueManager Instance;
+
+    [SerializeField] public int happinessEV;
+    [SerializeField] public int hotEV;
+    [SerializeField] Image emotionSprite;
+    [SerializeField] Sprite happySprite, neutralSprite, angrySprite, hotSprite,coldSprite;
+    public AHEmotion currentAHEmotion;
+    public HCEmotion currentHCEmotion;
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    public void ChangeEmotionValues(int change, bool isHappinessEV)
+    {
+        if (isHappinessEV)
+        {
+            happinessEV += change;
+        }
+        else
+        {
+            hotEV += change;
+        }
+        ChangeEmotion();
+        ChangeEmotionSprite();
+        ChangeEmotionName();
+        
+    }
+
+    void ChangeEmotion()
+    {
+        currentHCEmotion = hotEV == 0? HCEmotion.Neutral : hotEV > 0 ? HCEmotion.Hot : HCEmotion.Cold;
+        currentAHEmotion = happinessEV == 0 ? AHEmotion.Neutral : happinessEV > 0 ? AHEmotion.Happy : AHEmotion.Angry;
+
+    }
+    void ChangeEmotionName()
+    {
+        //Change the name on the phone based off hot/cold level
+    }
+    void ChangeEmotionSprite()
+    {
+        emotionSprite.sprite = happinessEV > 0 ? happySprite : angrySprite;
+    }
+}
+
+public enum AHEmotion
+{
+    Neutral,
+    Angry,
+    Happy
+
+}
+public enum HCEmotion
+{
+    Neutral,
+    Hot,
+    Cold
+}
---
+++ Assets/Scripts/Son/W-I-P/ReactionSO.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName ="Scriptables/ReactionSO")]
+public class ReactionSO: ScriptableObject
+{
+    public AHEmotion emotionNeeded;
+    public List<string> coldTextMessages;
+    public List<string> hotTextMessages;
+    public List<string> neutralTextMessages;
+}
